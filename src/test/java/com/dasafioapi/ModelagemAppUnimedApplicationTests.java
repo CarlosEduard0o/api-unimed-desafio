774594710cde34b800b1c4fb940eabb77e78a352
Carlos Eduardo <carlos_eduardo_a.r@outlook.com>
2024-07-30 18:33:29 -0300

    feat: first commit

---
+++ src/test/java/com/dasafioapi/ModelagemAppUnimedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasafioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemAppUnimedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
